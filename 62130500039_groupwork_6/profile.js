3d0e36647c8b05f29de79933864cae9645bcb3bb
zyct <thanyaluck.yict@mail.kmutt.ac.th>
2021-03-05 10:09:59 +0700

    [update] myprofile.html, profile.js

--- 62130500039_groupwork_6/profile.js
+++ 62130500039_groupwork_6/profile.js
@@ -1,26 +1,32 @@
 const constraints = {
-    username:{
+    username: {
         presence: true,
     },
-    email:{
+    email: {
         presence: true,
         email: true
     },
-    firstname:{
+    firstname: {
         presence: true,
     },
-    lastname:{
+    lastname: {
         presence: true,
     },
-    age:{
+    gender: {
+        presence: true,
+    },
+    age: {
         presence: true,
         numericality: {
             lessThan: 150
         }
+    },
+    department:{
+        presence: true
     }
 }
 
-const app = {
+const app = Vue.createApp( {
     data() {
         return {
             name: 'Thanyaluck Ngamchaipisit',
@@ -28,24 +34,59 @@ const app = {
             posts: '1039 posts',
             followers: '42.9k followers',
             following: '930 following',
-            formdata:{
+            formdata: {
                 username: null,
                 firstname: null,
                 lastname: null,
                 email: null,
+                gender: null,
                 age: null,
+                department: null
             },
-            errors: null
+            departmentList: [{department_id: 1, department_name: 'IT'},
+                        {department_id: 2, department_name: 'CS'},
+                        {department_id: 3, department_name: 'DSI'}
+            ],
+            errors: null,
+            submit: false
         }
     },
-    methods:{
-        checkForm(){
-            this.errors = validate(this.formdata,constraints)
-            if(!this.errors){
-                alert("Submit successfully")
+    methods: {
+        checkForm() {
+            this.errors = validate(this.formdata, constraints)
+            if (!this.errors) {
+                this.name = this.formdata.firstname + ' ' + this.formdata.lastname
+                this.id = '@' + this.formdata.username
+                this.submit = true
+                alert("Your profile is updated successfully.")
             }
         }
     }
-}
+})
 
-var mountedApp = Vue.createApp(app).mount('#app')
+app.component('display-error',{
+    props:{
+        errors:{
+            type: Object,
+            required: true
+        },
+        field:{
+            type:Object,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `
+    <div v-if="errors && errorList">
+        <p v-for="error in errorList" class="text-red-500"> {{ error }}</p>
+    </div>
+    `,
+    computed:{
+        errorList(){
+            return this.errors[this.field]
+        }
+    }
+})
+
+app.mount('#app')
